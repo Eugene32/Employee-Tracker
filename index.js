7122c1459f5cd39fb07f8a9746f76559c1e80d3e
Reynan Kanindot <ekahiyang@gmail.com>
2022-02-20 22:11:28 +1100

    completed queries

--- index.js
+++ index.js
@@ -23,18 +23,19 @@ var connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
 
-  console.log('connected as id ' + connection.threadId);
+    console.log('connected as id ' + connection.threadId);
 });
 
 
 
 mainMenu = [
+
     {
         type: 'list',
         name: 'mainMenu',
@@ -96,47 +97,86 @@ addEmployee = [
 
 ]
 
- const queryDepartments = () => {
-     const sql = `SELECT department.id AS ID,
+const queryDepartments = () => {
+    const sql = `SELECT department.id AS ID,
      department.name AS Department
      FROM department;`;
-     connection.query(sql, (err, rows) => {
-         if (err) throw err;
-         console.table(rows);
-        main();
-     });
- };
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.log('\r\n');
+        console.table(rows);
+        console.log('Press UP or DOWM key to continue.....');
+    });
+};
 
+const queryRoles = () => {
+    const sql = `SELECT role.id AS ID,
+    role.title AS Title
+    FROM role;`;
+    connection.query(sql, (err, roles) => {
+        if (err) throw err;
+        console.log('\r\n');
+        console.table(roles);
+        console.log('Press UP or DOWM key to continue.....');
+
+    });
+};
+
+
+
+const queryEmployees = () => {
+    const sql = `SELECT employee.id AS ID, 
+    CONCAT (employee.first_name, " ", employee.last_name) AS Name,
+    role.title AS Title, 
+    department.name AS Department,
+    role.salary AS Salary, 
+    CONCAT (manager.first_name, " ", manager.last_name) AS Manager
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id;`;
+    connection.query(sql, (err, employees) => {
+        if (err) throw err;
+        console.log('\r\n');
+        console.table(employees);
+        console.log('Press UP or DOWM key to continue.....');
+    });
+};
 
 async function main() {
     let mainMenuChoice = await inquirer.prompt(mainMenu);
 
     switch (mainMenuChoice.mainMenu) {
         case 'View All Departments':
-             const sql = await queryDepartments();
-            
+            const sqlDept = await queryDepartments();
+            process.stdout.write("\u001b[2J\u001b[0;0H");
+            main();
             break;
 
         case 'View All Roles':
-            // code block
+            const sqlRoles = await queryRoles();
+            process.stdout.write("\u001b[2J\u001b[0;0H");
+            main();
             break;
         case 'View All Employees':
-            // code blockg
+            const sqlEmployees = await queryEmployees();
+            process.stdout.write("\u001b[2J\u001b[0;0H");
+            main();
             break;
         case 'Add a Department':
             // code block
-             const newDept = await inquirer.prompt(addDepartment);
-             console.log(newDept);
+            const newDept = await inquirer.prompt(addDepartment);
+            console.log(newDept);
             break;
         case 'Add a Role':
             // code block
-             const newRole = await inquirer.prompt(addRole);
-             console.log(newRole);
+            const newRole = await inquirer.prompt(addRole);
+            console.log(newRole);
             break;
         case 'Add an Employee':
             // code block
-             const newEmployee = await inquirer.prompt(addEmployee);
-             console.log(newEmployee);
+            const newEmployee = await inquirer.prompt(addEmployee);
+            console.log(newEmployee);
             break;
         case 'Update an Employee Role':
             // code block
