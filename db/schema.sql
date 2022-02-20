8c36d397e083132132b5568e510a1a53792c6b79
Reynan Kanindot <ekahiyang@gmail.com>
2022-02-20 17:36:03 +1100

    created employees_db

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,35 @@
-DROP DATABASE IF EXISTS books_db;
--- Creates the "books_db" database --
-CREATE DATABASE books_db;
-
--- Use the books_db --
-USE books_db;
+DROP DATABASE IF EXISTS employees_db;
+-- Creates the  employees database --
+CREATE DATABASE employees_db;
+-- Use the employees_db --
+USE employees_db;
 
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(,2) NOT NULL,
-  department_id INT NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT ,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id:  INT NOT NULL,
-  manager_id INT ,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,12 +7,26 @@ VALUES
     ( 4, "Manufacturing"),
     ( 5, "Research and Development");
 
-    -- Insert department items --
+  
+    -- Insert role items --
 INSERT INTO role (id, title, salary, department_id)
 VALUES
-    ( 1, "Engrg Manager", 100,000.00, 3),
-    ( 2, "Engineer", 70,000.50, 3),
-    ( 3, "QC Inspector", 50,000.36, 2),
-    ( 4, "Mfg technician", 40,000.56, 4),
-    ( 5, "RnD Engineer", 70,000.56, 5),
-    ( 6, "QC Manager", 100,000.79, 2);
+    ( 1, "Engrg Manager", 100000, 1),
+    ( 2, "Engineer", 70000, 1),
+    ( 3, "QC Inspector", 50000, 2),
+    ( 4, "Mfg technician", 40000, 4),
+    ( 5, "RnD Engineer", 70000, 5),
+    ( 6, "QC Manager", 100000, 2);
+
+
+  -- Insert employee items --
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+     ( 1, "Roy", "Gibson", 1, 0 ),
+    ( 2, "Raul", "Genson", 2, 1),
+    ( 3, "Razer", "Hammock", 6, 0 ),
+    ( 4, "Kenneth", "Sy", 3, 3),
+    ( 5, "Kenneth", "Do", 3, 3),
+     ( 6, "Kenneth", "Tok", 3, 3),
+     ( 7, "Kenneth", "Wok", 3, 3),
+     ( 8, "Wei", "Tulow", 2, 1);
